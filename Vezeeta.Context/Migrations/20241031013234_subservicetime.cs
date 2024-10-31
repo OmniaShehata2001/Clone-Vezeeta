1ce7edf2631c978757b0c15502e42a15adb27fc5
Omnia <omniashehata36@gmail.com>
2024-10-31 04:34:04 +0300

    SubServiceTimeSlot Migration

---
+++ Vezeeta.Context/Migrations/20241031013234_subservicetime.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vezeeta.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class subservicetime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBooked",
+                table: "SubServicesTimeSlots",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBooked",
+                table: "SubServicesTimeSlots");
+        }
+    }
+}
